23b17224e43b249ca8ef350f2dfc5b799950b253
daniehacks <daniebanks718@gmail.com>
2014-04-09 23:23:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TellerScheduler::Application.config.secret_key_base = 'e686bc799f2e856c2cc4d9a1c112b41aaba10047b547e77c9260b9fff95bce15c89abd8ad78582d30b53a5dcbec83c007eb1cc74e92b15257afabb197d1d4b45'
